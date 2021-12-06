ee4fa9cb88370d611aab044c128c99d83dbb79cd
Simon Reitinger <simre4775@gmail.com>
2021-12-06 09:38:30 +0100

    Add day 6

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ My solutions to the coding problems in Advent of Code 2021.
 - [x] Day 3
 - [x] Day 4
 - [x] Day 5
-- [ ] Day 6
+- [x] Day 6
 - [ ] Day 7
 - [ ] Day 8
 - [ ] Day 9
--- src/Day6/Main.hs
+++ src/Day6/Main.hs
@@ -1,7 +1,31 @@
 module Day6.Main where
 
+import Data.List (group, sort)
+import Data.List.Split (splitOn)
+import qualified Data.Vector as V
+
+type Population = V.Vector Int
+
+parseInitialPopulation :: String -> Population
+parseInitialPopulation input =
+  let fish = read @Int <$> splitOn "," input
+   in V.fromList $ pred . length <$> group (sort ([0 .. 8] ++ fish))
+
+simulatePopulation :: Population -> Population
+simulatePopulation previousPopulation =
+  V.generate populationClasses (\popClass -> sum $ (previousPopulation V.!) <$> from popClass)
+  where
+    populationClasses = 9
+    from 6 = [0, 7]
+    from 8 = [0]
+    from i = [succ i]
+
 solution1 :: IO ()
-solution1 = pure ()
+solution1 = do
+  initialPopulation <- parseInitialPopulation <$> readFile "inputs/day6/input1"
+  print $ V.sum $ (!! 80) $ iterate simulatePopulation initialPopulation
 
 solution2 :: IO ()
-solution2 = pure ()
+solution2 = do
+  initialPopulation <- parseInitialPopulation <$> readFile "inputs/day6/input1"
+  print $ V.sum $ (!! 256) $ iterate simulatePopulation initialPopulation
