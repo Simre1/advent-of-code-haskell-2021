804e72a963338203d05999cde1efafe0eb5de2c4
Simon Reitinger <simre4775@gmail.com>
2021-12-12 11:19:31 +0100

    Add day 12

--- src/Day12/Main.hs
+++ src/Day12/Main.hs
@@ -1,7 +1,48 @@
 module Day12.Main where
 
+import Algorithm.Search
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.State
+import Data.Char
+import Data.List
+import Data.List.Split
+import qualified Data.Map as M
+import Data.Maybe
+import qualified Data.Set as S
+import Debug.Trace
+
+type CaveSystem = M.Map String [String]
+
+parseInput :: String -> CaveSystem
+parseInput = foldl' parseLine M.empty . lines
+  where
+    parseLine m str = let [start, end] = splitOn "-" str in M.alter (\n -> pure $ start : fromMaybe [] n) end $ M.alter (\n -> pure $ end : fromMaybe [] n) start m
+
+listAllPaths :: Bool -> CaveSystem -> [[String]]
+listAllPaths visitTwice caveSystem = flip execState [] $ dfsM nextStates isGoal initialState
+  where
+    initialState = (S.empty, [], visitTwice, "start")
+    isGoal _ = pure False
+    nextStates :: (S.Set String, [String], Bool, String) -> State [[String]] [(S.Set String, [String], Bool, String)]
+    nextStates (visited, path, canVisitTwice, currentNode) =
+      if currentNode == "end"
+        then modify (("end" : path) :) >> pure []
+        else do
+          let path' = currentNode : path
+              visited' = if isLower (head currentNode) then S.insert currentNode visited else visited
+              nextNodes =
+                let next = filter (/= "start") $ fromMaybe [] $ M.lookup currentNode caveSystem
+                 in if canVisitTwice
+                      then ((False,) <$> filter (`S.member` visited) next) <> ((True,) <$> filter (`S.notMember` visited) next)
+                      else (False,) <$> filter (`S.notMember` visited) next
+           in pure $ (\(a, b) -> (visited', path', a, b)) <$> nextNodes
+
 solution1 :: IO ()
-solution1 = pure ()
+solution1 = do
+  caveSystem <- parseInput <$> readFile "inputs/day12/input1"
+  print $ length $ listAllPaths False caveSystem
 
 solution2 :: IO ()
-solution2 = pure ()
+solution2 = do
+  caveSystem <- parseInput <$> readFile "inputs/day12/input1"
+  print $ length $ listAllPaths True caveSystem
