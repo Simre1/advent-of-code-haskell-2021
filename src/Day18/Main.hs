cd88bc7b4ad3402604814c262dcb973edd2fc27b
Simon Reitinger <simre4775@gmail.com>
2021-12-19 13:25:27 +0100

    Add day 19

--- src/Day18/Main.hs
+++ src/Day18/Main.hs
@@ -1,7 +1,77 @@
 module Day18.Main where
 
+import Control.Applicative
+import Control.Monad
+import Data.Foldable
+import Data.Void (Void)
+import qualified Text.Megaparsec as P
+import qualified Text.Megaparsec.Char as P
+import qualified Text.Megaparsec.Char.Lexer as P
+import Debug.Trace (traceShowId)
+
+data Number = Pair Number Number | Lit Int deriving (Show, Eq)
+
+parse :: String -> [Number]
+parse = either (error . P.errorBundlePretty) id . P.parse p "target value"
+  where
+    p :: P.Parsec Void String [Number]
+    p = many $ number >>= (<$ (void P.newline <|> P.eof))
+    number =
+      pair <|> lit
+    lit = Lit <$> P.decimal
+    pair = do
+      P.char '['
+      n1 <- number
+      P.char ','
+      n2 <- number
+      P.char ']'
+      pure $ Pair n1 n2
+
+addNumbers :: Number -> Number -> Number
+addNumbers n1 n2 = reduceNumber $ Pair n1 n2
+
+reduceNumber :: Number -> Number
+reduceNumber n = maybe n reduceNumber $ explode n <|> split n
+
+fromLit :: Number -> Int
+fromLit (Lit n) = n
+fromLit _ = error "Tried to extract Lit from Pair"
+
+explode :: Number -> Maybe Number
+explode = go (\_ n _ -> Just n) 0
+  where
+    go f _ (Lit a) = Nothing
+    go f d (Pair a b)
+      | d < 4 =
+        go (\l n r -> f l (Pair n $ addLeftMost r b) 0) (succ d) a
+          <|> go (\l n r -> f 0 (Pair (addRightMost l a) n) r) (succ d) b
+      | otherwise = f (fromLit a) (Lit 0) (fromLit b)
+    addLeftMost 0 n = n
+    addLeftMost i (Lit n) = Lit $ i + n
+    addLeftMost i (Pair a b) = Pair (addLeftMost i a) b
+    addRightMost 0 n = n
+    addRightMost i (Lit n) = Lit $ i + n
+    addRightMost i (Pair a b) = Pair a (addRightMost i b)
+
+split :: Number -> Maybe Number
+split = go
+  where 
+    go (Lit i)
+      | i > 9 = Just $ Pair (Lit (i `quot` 2)) (Lit . abs $ i `div` (-2))
+      | otherwise = Nothing
+    go (Pair a b) =
+      flip Pair b <$> go a <|> Pair a <$> go b
+
+magnitude :: Number -> Int
+magnitude (Lit a) = a
+magnitude (Pair a b) = 3 * magnitude a + 2 * magnitude b
+
 solution1 :: IO ()
-solution1 = pure ()
+solution1 = do
+  input <- parse <$> readFile "inputs/day18/input1"
+  print $ magnitude $ foldl1 addNumbers input
 
 solution2 :: IO ()
-solution2 = pure ()
+solution2 = do
+  input <- parse <$> readFile "inputs/day18/input1"
+  print $ maximum $ [max (magnitude (addNumbers n1 n2)) (magnitude (addNumbers n2 n1)) | n1 <- input, n2 <- input, n1 /= n2]
--- src/Day19/Main.hs
+++ src/Day19/Main.hs
@@ -1,7 +1,94 @@
 module Day19.Main where
 
+import Control.Applicative ( Applicative(liftA2) )
+import Data.List.Split (splitOn)
+import qualified Data.Map.Strict as M
+import Linear.V3 ( V3(..) )
+import qualified Data.Set as S
+
+data Coords = X | Y | Z deriving (Show, Eq, Enum, Ord)
+
+data Direction a = Forward a | Backward a deriving (Show, Eq, Ord)
+
+data Orientation = Orientation (V3 (Direction Coords)) deriving (Show, Eq, Ord)
+
+data Normalised = Normalised Int Int Int
+
+data Scanner = Scanner (V3 Int) [V3 Int] deriving (Show, Eq)
+
+readInput :: String -> [[V3 Int]]
+readInput str =
+  let scanners = splitOn "\n\n" str
+   in fmap ((\[a, b, c] -> V3 a b c) . fmap read . splitOn ",") . tail . lines <$> scanners
+
+applyOrientation :: Orientation -> V3 Int -> V3 Int
+applyOrientation (Orientation (V3 dx dy dz)) (V3 x y z) = f dx x + f dy y + f dz z
+  where
+    f d n =
+      let (m, c) = case d of
+            Forward a -> (1, a)
+            Backward a -> (-1, a)
+       in case c of
+            X -> V3 (m * n) 0 0
+            Y -> V3 0 (m * n) 0
+            Z -> V3 0 0 (m * n)
+
+allOrientations :: [Orientation]
+allOrientations =
+  [ Orientation $ V3 (fx x) (fy y) (fz z)
+    | fx <- directions,
+      fy <- directions,
+      fz <- directions,
+      x <- coords,
+      y <- coords,
+      x /= y,
+      z <- coords,
+      y /= z,
+      x /= z
+  ]
+  where
+    coords = [X, Y, Z]
+    directions = [Forward, Backward]
+
+matchScanner :: Scanner -> [V3 Int] -> Maybe Scanner
+matchScanner (Scanner pos readings) unknownReadings =
+  case foldr fold (Left M.empty) allPossibilities of
+    Left _ -> Nothing
+    Right (position, orientation) -> Just $ Scanner position (applyOrientation orientation <$> unknownReadings)
+  where
+    fold :: (V3 Int, (V3 Int, Orientation)) -> Either (M.Map (V3 Int, Orientation) Int) (V3 Int, Orientation) -> Either (M.Map (V3 Int, Orientation) Int) (V3 Int, Orientation)
+    fold _ (Right a) = Right a
+    fold (r, (u, o)) (Left m) = case M.lookup (pos + r - u, o) m of
+      Nothing -> Left $ M.insert (pos + r - u, o) 1 m
+      Just i -> if i >= 11 then Right (pos + r - u, o) else Left $ M.insert (pos + r - u, o) (succ i) m
+    allPossibilities = liftA2 (,) readings ((\u o -> (applyOrientation o u, o)) <$> unknownReadings <*> allOrientations)
+
+findAllScanners :: Scanner -> [[V3 Int]] -> [Scanner]
+findAllScanners scanner0 = go [scanner0]
+  where
+    go [] _ = []
+    go scanners [] = scanners
+    go (s : scanners) readings =
+      let (foundScanners, undeterminedReadings) = findOverlapping s readings
+       in s : go (scanners ++ foundScanners) undeterminedReadings
+    findOverlapping s [] = ([], [])
+    findOverlapping s (r : rs) =
+      let (foundScanners, undeterminedReadings) = findOverlapping s rs
+       in maybe (foundScanners, r : undeterminedReadings) (\f -> (f : foundScanners, undeterminedReadings)) (matchScanner s r)
+
 solution1 :: IO ()
-solution1 = pure ()
+solution1 = do
+  input <- readInput <$> readFile "inputs/day19/input1"
+  let scanner0 = Scanner (V3 0 0 0) $ head input
+      scanners = findAllScanners scanner0 $ tail input
+  print $ S.size $ S.fromList $ foldMap (\(Scanner p readings) -> (p+) <$> readings) scanners
+
 
 solution2 :: IO ()
-solution2 = pure ()
+solution2 = do
+  input <- readInput <$> readFile "inputs/day19/input1"
+  let scanner0 = Scanner (V3 0 0 0) $ head input
+      scanners = findAllScanners scanner0 $ tail input
+      positions = (\(Scanner pos _) -> pos) <$> scanners
+  print $ maximum $ [sum $ abs (p2 - p1)| p1 <- positions, p2 <- positions]
+
